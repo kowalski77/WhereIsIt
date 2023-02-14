4fda29e91705820a6210a3eea6f70314e0637980
Christian Aranda <kristian.aranda@gmail.com>
2023-02-14 18:52:19 +0100

    Refactor

--- WhereIsIt/Domain/EntriesProcessing/RuleBasedProcessor.cs
+++ WhereIsIt/Domain/EntriesProcessing/RuleBasedProcessor.cs
@@ -10,32 +10,3 @@ internal abstract class RuleBasedProcessor : IEntryProcessor
     public IEnumerable<Entry> Execute(IEnumerable<Line> lines) =>
         lines.Select(this.Splitter.ApplyTo).Map();
 }
-
-internal class EntriesContentSplitter : RuleBasedProcessor
-{
-    protected override IMultiwaySplitter Splitter =>
-        CharacterSplitter.SemicolonSpliter
-        .Append(CharacterSplitter.CommaSpliter);
-}
-
-internal class CharacterSplitter : IMultiwaySplitter
-{
-    private char Character { get; }
-
-    private CharacterSplitter(char character) => this.Character = character;
-
-    public Maybe<Entry> ApplyTo(Line line)
-    {
-        var parts = line.Value.Split(this.Character);
-        if (parts.Length < 2)
-        {
-            return None.Value;
-        }
-
-        return Entry.New(parts[0], parts[1], parts[2]);
-    }
-
-    public static IMultiwaySplitter SemicolonSpliter => new CharacterSplitter(';');
-
-    public static IMultiwaySplitter CommaSpliter => new CharacterSplitter(',');
-}
